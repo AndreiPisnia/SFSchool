8dbaa6e02053807e594e436e17449c8c3926e8a2
AndreiPisnia <43684277+AndreiPisnia@users.noreply.github.com>
2018-10-28 13:16:04 +0200

    Add files via upload

---
+++ 1_2 100years.py
@@ -0,0 +1,9 @@
+
+name = input('What is you name?')
+age = int (input ('How old are you?'))
+birthday100 = 2018+(100 - age)
+
+print ('Hello, {} !' .format(name))
+print ('You are {} years old.' .format(age))
+
+print ('You will selebrate the 100-th birthday in', birthday100, 'year!')
